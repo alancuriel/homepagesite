9d68eaf67f4f503684b4b28114d9936a37f80c57
Alan Curiel <alannitro17@gmail.com>
2020-07-19 16:04:35 -0400

    Add page navigation for aliases
    Added website navigation with
    aliases and a clearing alisases

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,52 @@
-import React from "react"
+import React, { useState } from "react"
 
 import "../styles/global.css"
 
 import settings from "../../static/home-config.json"
 
 export default function Home() {
+  const [inputText, setInputText] = useState("s")
+  const [history, setHistory] = useState([{ key: 1, value: "cd" }])
+
+  const keyDownHandler = e => {
+    if (e.keyCode === 13) {
+      runCommand()
+    }
+  }
+
+  const runCommand = () => {
+    const text = inputText;
+    if(settings.aliases.clearAliases.includes(text)) {
+      clearConsole();
+      return;
+    } else {
+
+      const i = settings.aliases.redirectAliases.findIndex((e) => {
+       return e.aliases.includes(text);
+      });
+
+      if (i >= 0) {
+        window.location.href = settings.aliases.redirectAliases[i].link;
+      }
+
+    }
+
+    addHistoryItem(settings.general.shellPrompt + " " + text);
+    setInputText("");
+  };
+
+  const clearConsole = () => {
+    setHistory([])
+    setInputText("")
+  };
+
+  const addHistoryItem = (text) => {
+    setHistory([...history, {
+      key: Math.round(Math.random * 1000000),
+      value: text
+    }]);
+  };
+
   return (
     <div className="container">
       <div
@@ -19,17 +61,29 @@ export default function Home() {
           backgroundColor: settings.style.consoleBackgroundColor,
         }}
       >
+        {history.map(item => {
+          return <p className="historyItem" key={item.key} style={{
+            color: settings.style.consoleTextColor
+          }}
+          >
+            {item.value}
+            </p>
+        })}
+
         <p
           className="shell"
           style={{
-            color: settings.style.consoleUsernameColor,
+            color: settings.style.consolePromptColor,
           }}
         >
-          {settings.general.username}~&nbsp;
+          {settings.general.shellPrompt}&nbsp;
           <input
             className="consoleInput"
             spellCheck="false"
             style={{ color: settings.style.consoleTextColor }}
+            value={inputText}
+            onChange={e => setInputText(e.target.value)}
+            onKeyDown={keyDownHandler}
           ></input>
         </p>
       </div>
