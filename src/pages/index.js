4d1c166beacfd354b6d4871023f84c93e75ee911
Alan Curiel <alancuriel@Alans-MacBook-Pro.local>
2020-07-19 10:55:32 -0400

    Add project files
    Has the layout of the application, but no functionality

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,38 @@
 import React from "react"
 
+import "../styles/global.css"
+
+import settings from "../../static/home-config.json"
+
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div className="container">
+      <div
+        className="content"
+        style={{
+          width: settings.style.consoleWidth,
+          height: settings.style.consoleHeight,
+          borderRadius: settings.style.consoleBorderRadius,
+          backdropFilter: "blur(" + settings.style.consoleBlurStrength + ")",
+          WebkitBackdropFilter:
+            "blur(" + settings.style.consoleBlurStrength + ")",
+          backgroundColor: settings.style.consoleBackgroundColor,
+        }}
+      >
+        <p
+          className="shell"
+          style={{
+            color: settings.style.consoleUsernameColor,
+          }}
+        >
+          {settings.general.username}~&nbsp;
+          <input
+            className="consoleInput"
+            spellCheck="false"
+            style={{ color: settings.style.consoleTextColor }}
+          ></input>
+        </p>
+      </div>
+    </div>
+  )
 }
